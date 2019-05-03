5d57b324289018949078d23665d8b5c0a41b6821
GekidouX <47258390+theclegginator@users.noreply.github.com>
2019-05-03 15:36:05 -0400

    Added default LIRI prompt and added log file functionality

--- liri.js
+++ liri.js
@@ -5,14 +5,29 @@ const keys = require("./keys.js");
 const axios = require("axios"); // enable axios for API calls within Node.
 const spotify = new Spotify(keys.spotify);
 const moment = require("moment");
-const fs = require("file-system");
+const fs = require("file-system"); 
+ 
+// Function for writing LIRI data to a log file
+function writeToFile(data) {
+    let timeStamp = `${moment()}\n`;
+    fs.appendFile('liri_log.txt',timeStamp, 'utf8',
+        // callback function for error loggin
+        function(err) { 
+            if (err) throw err;
+            // If there is no error writing the timestamp, then write the data to the log.
+            fs.appendFile('liri_log.txt',data, 'utf8',
+                function(err) { 
+                    if (err) throw err;
+    });
+    });
+}
 
 // ========================== //
 // ===== LIRI FUNCTIONS ===== //
 // ========================== //
 
 // ========================== //
-// ===== Bands in Town ===== //
+// ======= Bandsintown ====== //
 // ========================== //
 function concert(artist="Periphery") {
     console.log("\x1b[35m", `===== Retrieving Information on Next Three Concerts for ${artist}... =====`)
@@ -20,27 +35,29 @@ function concert(artist="Periphery") {
     axios
     .get(queryURL)
     .then(function(response) {
-        console.log("\x1b[33m", 
-                `Event Info for ${artist} Retrieved:
-                ===== Event 1 =====
-                Date: ${moment(response.data[0].datetime).format("MM/DD/YYYY")}
-                Venue: ${response.data[0].venue.name}
-                Country: ${response.data[0].venue.country}
-                City: ${response.data[0].venue.city}
+        let data = 
+            `Event Info for ${artist} Retrieved:
+            ===== Event 1 =====
+            Date: ${moment(response.data[0].datetime).format("MM/DD/YYYY")}
+            Venue: ${response.data[0].venue.name}
+            Country: ${response.data[0].venue.country}
+            City: ${response.data[0].venue.city}
 
-                ===== Event 2 =====
-                Date: ${moment(response.data[1].datetime).format("MM/DD/YYYY")}
-                Venue: ${response.data[1].venue.name}
-                Country: ${response.data[1].venue.country}
-                City: ${response.data[1].venue.city}
+            ===== Event 2 =====
+            Date: ${moment(response.data[1].datetime).format("MM/DD/YYYY")}
+            Venue: ${response.data[1].venue.name}
+            Country: ${response.data[1].venue.country}
+            City: ${response.data[1].venue.city}
 
-                ===== Event 3 =====
-                Date: ${moment(response.data[2].datetime).format("MM/DD/YYYY")}
-                Venue: ${response.data[2].venue.name}
-                Country: ${response.data[2].venue.country}
-                City: ${response.data[2].venue.city}`
-            )
-            console.log("\x1b[0m", "") // resets terminal color to default.
+            ===== Event 3 =====
+            Date: ${moment(response.data[2].datetime).format("MM/DD/YYYY")}
+            Venue: ${response.data[2].venue.name}
+            Country: ${response.data[2].venue.country}
+            City: ${response.data[2].venue.city}\n`
+        console.log("\x1b[33m", data)
+        console.log("\x1b[0m", "") // resets terminal color to default.
+        // write the retrieved data to a text file log.
+        writeToFile(data);
     })
     // If any errors are present, show them in the console log.
     .catch(function(error) {
@@ -58,22 +75,24 @@ function concert(artist="Periphery") {
 }
 
 // ========================== //
-// ===== Spotify ===== //
+// ======== Spotify ========= //
 // ========================== //
 function spotifyCall(song = "The Sign Ace of Base") {
     console.log("\x1b[35m", `===== Retrieving Song Information for ${song}... =====`)
+    // perform search using the spotify node module, then pull specific info by digging into JSON packet from promise.
     spotify
     .search({ type: 'track', query: `${song}` })
     .then(function(response) {
-      //console.log(response.tracks.items[0])
-      console.log("\x1b[32m", 
-                `Song Info Retrieved:
-                Artist: ${response.tracks.items[0].artists[0].name}
-                Song Title: ${response.tracks.items[0].name}
-                Album Title: ${response.tracks.items[0].album.name}
-                Song Link: ${response.tracks.items[0].external_urls.spotify}`
-            )
-            console.log("\x1b[0m", "") // Resets terminal color to default.
+        let data = 
+            `Song Info Retrieved:
+            Artist: ${response.tracks.items[0].artists[0].name}
+            Song Title: ${response.tracks.items[0].name}
+            Album Title: ${response.tracks.items[0].album.name}
+            Song Link: ${response.tracks.items[0].external_urls.spotify}\n`
+        console.log("\x1b[32m", data)
+        console.log("\x1b[0m", "") // Resets terminal color to default.
+        // write the retrieved data to a text file log.
+        writeToFile(data);
     })
     .catch(function(err) {
       console.log(err);
@@ -81,7 +100,7 @@ function spotifyCall(song = "The Sign Ace of Base") {
 }
 
 // ========================== //
-// ===== OMDB ===== //
+// ========== OMDB ========== //
 // ========================== //
 
 function omdb(movieTitle = "mr+nobody") {
@@ -94,18 +113,21 @@ function omdb(movieTitle = "mr+nobody") {
     axios
     .get(queryURL)
     .then(function(response) {
-        console.log("\x1b[36m%s\x1b[0m", 
-                `Movie Info Retrieved:
-                Title: ${response.data.Title}
-                Released: ${response.data.Year}
-                IMDB Score: ${response.data.imdbRating}
-                Rotten Tomatoes Score: ${response.data.Ratings[1].Value}
-                Country of Origin: ${response.data.Country}
-                Language: ${response.data.Language}
-                Plot Summary: ${response.data.Plot}
-                Main Actors: ${response.data.Actors}`
-            )
-            console.log("\x1b[0m", "") // resets terminal color to default.
+        let data = 
+            `Movie Info Retrieved:
+            Title: ${response.data.Title}
+            Released: ${response.data.Year}
+            IMDB Score: ${response.data.imdbRating}
+            Rotten Tomatoes Score: ${response.data.Ratings[1].Value}
+            Country of Origin: ${response.data.Country}
+            Language: ${response.data.Language}
+            Plot Summary: ${response.data.Plot}
+            Main Actors: ${response.data.Actors}\n`
+
+        console.log("\x1b[36m%s\x1b[0m", data) 
+        console.log("\x1b[0m", "") // resets terminal color to default.
+        // write the retrieved data to a text file log.
+        writeToFile(data);
     })
     // If any errors are present, show them in the console log.
     .catch(function(error) {
@@ -121,11 +143,16 @@ function omdb(movieTitle = "mr+nobody") {
         console.log(error.config);
     });
 }
+
 // ========================== //
 // ===== Read Text File ===== //
 // ========================== //
 function randomize() {
     fs.readFile("random.txt", function(err, content) {
+        // As a JSON object or a string is a "truthy" value, while null is not, we can call the error below if one exists:
+        if (err) {
+            console.log(err)
+        }
         let liriFunction = content.toString().split(",")[0]; // gathers the liri function from the text file
         let liriParam = content.toString().split(",")[1]; // gathers the liri parameter (like song title, etc.)
         liriParam = liriParam.replace(/['"]+/g, ''); //removes any quotes around the liriParameter
@@ -150,8 +177,30 @@ function randomize() {
 // take in the LIRI command.
 // Will take format "node liri.js [command]"
 let liriCommand = process.argv[2]; 
+console.log(liriCommand)
 
 switch (liriCommand) {
+    // if no LIRI function was called, tell the user how to operate LIRI.
+    case undefined: 
+        console.log("\x1b[35m",`
+        Welcome to LIRI (Language Interpretation and Recognition Interface)!\n
+        ===== COMMANDS FOR LIRI =====
+        1. Look up concerts for an artist.
+            - Use the format node 'liri.js concert-this "<artist or band name here>"'
+            - Will return information from Bandsintown for the next 3 upcoming shows from the artist specified.
+        2. Look up song info on Spotify.
+            - Use the format node 'liri.js spotify-this-song "<song name here>"'
+            - Will return information and a spotify link for the best search result matching your request.
+        3. Look up information on a movie.
+            - Use the format node 'liri.js movie-this "<movie name here>"'
+            - Will return information like actors, year, and review scores for the movie specified.
+            4. Follow a command from a file named "random.txt" from the same working directory.
+            - Use the format node 'liri.js do-what-it-says'
+            - You can place a LIRI command and query info into random.text just as you would for any of the above 3 commands.
+        ============================
+        `)
+        console.log("\x1b[0m", "")
+        break;
     case "concert-this":
         // input format = `node liri.js concert-this <artist/band name here>`
         let artist = process.argv[3];
